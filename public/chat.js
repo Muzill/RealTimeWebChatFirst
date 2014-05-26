0aedf371e34dc031aae30fa1d0563e34f3c3b2aa
Muzill <concept_alfa@hotmail.com>
2014-05-26 12:24:58 -0700

    Fixed $

--- public/chat.js
+++ public/chat.js
@@ -33,10 +33,3 @@ window.onload = function() {
     };
  
 }
-$(document).ready(function() {
-    $("#field").keyup(function(e) {
-        if(e.keyCode == 13) {
-            sendMessage();
-        }
-    });
-});
