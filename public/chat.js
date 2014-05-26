a85212bba9796bf89ef41d56b431e34ddcd0554d
Muzill <concept_alfa@hotmail.com>
2014-05-26 12:35:42 -0700

    Fixed connection issue

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://localhost:3700');
+    var socket = io.connect('http://conceptalfa.noip.me:3700');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
